c26fa316db5d4bd2b81b672cff312bda323a601f
Unknown <22047487+delta1512@users.noreply.github.com>
2018-05-01 18:55:42 +1000

    Fixed bugs

--- Notetaker.py
+++ Notetaker.py
@@ -17,19 +17,20 @@ class notetaker:
         self.uid = user
         self.uname = username
         self.root = '{}{}/'.format(c.root, user)
-        if os.path.exists(self.root):
+        if not os.path.exists(self.root):
             os.mkdir(self.root)
         self.files = [f.split(sep='/')[-1] for f in iglob(self.root + '*')]
 
     def ls(self):
         acc = '```File Name                 Line Count\n'
         for f in self.files:
-            line_count = str(sum([1 for line in open(f, 'r')]))
-            acc += f + '\t'*5 + line_count + '\n'
+            line_count = str(sum([1 for line in open(self.root+f, 'r')]))
+            acc += f + '\t'*6 + line_count + '\n'
         return acc + '```'
 
     def open_context(self, fname):
         fname = fname.replace('/', '').replace('..', '')
+        self.context = self.root + fname
         if not fname in self.files:
             self.files.append(fname)
             self.write_to_context(co.IDENT)
@@ -41,16 +42,16 @@ class notetaker:
         return e.GOOD[:-3]
 
     def write_to_context(self, type, data=''):
-        if context is None:
+        if self.context is None:
             return '{}There is currenly no open file, use the `open` command'.format(e.CANNOT)
         with open(self.context, 'a') as f:
             if type == co.IDENT:
-                ctime = time.time()
+                ctime = round(time.time())
                 w = co.HEADER.format(
                     self.context.split(sep='/')[-1],
                     self.uname,
-                    time.strftime('%d-%m-%Y %H:%M:%S', ctime),
-                    ctime))
+                    time.strftime('%d-%m-%Y', time.gmtime(ctime)),
+                    ctime)
             elif type == co.TOPIC:
                 w = '### {}\n'.format(data)
             elif type == co.TXT:
@@ -58,6 +59,15 @@ class notetaker:
             f.write(w)
             self.latest_changes.append({deepcopy(self.context) : w})
             return 0
+
+    def read_context(self):
+        if self.context is None:
+            return '{}There is currenly no open file, use the `open` command'.format(e.CANNOT)
+        with open(self.context, 'r') as f:
+            data = f.read()
+        if len(data) >= 2000:
+            return '{}File too big to show'.format(e.ERROR)
+        return '```{}```'.format(data)
 
     def delete(self, filename):
         if not filename in self.files:
@@ -74,3 +84,4 @@ class notetaker:
             with open(key, 'w') as f:
                 f.write(tmp.replace(latest_change[key], ''))
             return '{}Undo process was successful. Changed: {}'.format(e.GOOD, key.split(sep='/')[-1])
+        return '{}There are no more changes that can be undone'.format(e.INFO)
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import emotes as e
 
 import asyncio
 import discord
+import discord
+from discord.utils import get
 
-pre = '"'
+pre = '%'
 client = discord.Client()
 UDB = {}
 
@@ -14,7 +16,7 @@ async def on_message(msg):
     cmd = msg.content
     chan = msg.channel
     user = msg.author.id
-    if cmd.startswith(pre) and chan.is_private:
+    if cmd.startswith(pre):
         cmd = cmd[1:]
         if not user in UDB:
             UDB[user] = n(user, msg.author.name)
@@ -22,34 +24,42 @@ async def on_message(msg):
         if cmd.split()[0] in ['new', 'open']:
             args = cmd.split()[1:]
             if 0 < len(args) < 2:
-                client.send_message(chan, NOTES.open_context(args[0]))
+                await client.send_message(chan, NOTES.open_context(args[0]))
             else:
-                client.send_message(chan, '{}Invalid arguments'.format(e.ERROR))
+                await client.send_message(chan, '{}Invalid arguments'.format(e.ERROR))
         elif cmd.startswith('topic'):
             exit_code = NOTES.write_to_context(co.TOPIC, data=cmd[6:])
             if exit_code == 0:
-                client.add_reaction(msg, ':white_check_mark:')
+                await client.add_reaction(msg, '✅')
             else:
-                client.send_message(chan, exit_code)
+                await client.send_message(chan, exit_code)
         elif cmd.startswith('close'):
-            client.send_message(chan, NOTES.close_context())
+            await client.send_message(chan, NOTES.close_context())
+        elif cmd.startswith('read'):
+            await client.send_message(chan, NOTES.read_context())
         elif cmd.startswith('delete'):
             args = cmd.split()[1:]
             if 0 < len(args) < 2:
-                client.send_message(chan, NOTES.delete(args[0]))
+                await client.send_message(chan, NOTES.delete(args[0]))
             else:
-                client.send_message(chan, '{}Invalid arguments'.format(e.ERROR))
+                await client.send_message(chan, '{}Invalid arguments'.format(e.ERROR))
         elif cmd.startswith('status'):
             current = NOTES.context
             if current is None:
                 current = '{}No file is open'.format(e.INFO)
             else:
-                current = current.split(sep='/')[-1]
-            client.send_message(chan, current)
+                current = 'Document: {}'.format(current.split(sep='/')[-1])
+            await client.send_message(chan, current)
         elif cmd.startswith('ls'):
-            client.send_message(chan, NOTES.ls())
+            await client.send_message(chan, NOTES.ls())
         elif cmd.startswith('undo'):
-            client.send_message(chan, NOTES.undo())
+            await client.send_message(chan, NOTES.undo())
+    elif chan.is_private and user in UDB:
+        out = UDB[user].write_to_context(co.TXT, data=cmd)
+        if out == 0:
+            await client.add_reaction(msg, '✅')
+        else:
+            await client.send_message(chan, out)
 
 try:
     with open('API.key', 'r') as key_file:
